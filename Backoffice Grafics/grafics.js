984d23a6d46c5dd20c21c5eb4b202296e09417e0
ViictorAC <victorach007@gmail.com>
2026-05-04 23:09:54 +0200

    VAAA QUE JA HO TENIM XIQUETS

--- Backoffice Grafics/grafics.js
+++ Backoffice Grafics/grafics.js
@@ -6,7 +6,7 @@ new Chart(document.getElementById("grafic-redo"), {
       datasets: [{
         label: "Population (millions)",
         backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,800]
+        data: [150,300,50]
       }]
     },
     options: {
